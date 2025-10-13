sidux/connexion

commit 8e8a89db58c855375a3d8455f5bb1f16ed978944

Drop Python 3.9 and add 3.13, 2.14 (#2088)

.github/workflows/pipeline.yml
@@ -9,7 +9,7 @@ jobs:
     runs-on: ubuntu-latest
     strategy:
       matrix:
-        python-version: ['3.8', '3.9', '3.10', '3.11', '3.12']
+        python-version: ['3.9', '3.10', '3.11', '3.12', '3.13', '3.14'']
     steps:
     - uses: actions/checkout@v2
     - name: Set up Python ${{ matrix.python-version }}

connexion/utils.py
@@ -499,7 +499,7 @@ def sort_routes(routes, *, key=None):
             self.path_regex, _, _ = compile_path(self.path)
 
         def __lt__(self, other: "SortableRoute") -> bool:
-            return bool(other.path_regex.match(self.path))
+            return bool(other.path_regex.match(self.path)) or self.path < other.path
 
     return sorted(routes, key=lambda r: SortableRoute(key(r) if key else r))
 

pyproject.toml
@@ -26,11 +26,12 @@ classifiers = [
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3 :: Only",
-    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
+    "Programming Language :: Python :: 3.14",
     "Topic :: Internet",
     "Topic :: Internet :: WWW/HTTP",
     "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
@@ -44,7 +45,7 @@ classifiers = [
 connexion = 'connexion.cli:main'
 
 [tool.poetry.dependencies]
-python = '^3.8'
+python = '^3.9'
 asgiref = ">= 3.4"
 httpx = ">= 0.23"
 inflection = ">= 0.3.1"

.github/workflows/release.yml
@@ -13,10 +13,10 @@ jobs:
     steps:
     - uses: actions/checkout@master
 
-    - name: Set up Python 3.9
+    - name: Set up Python 3.12
       uses: actions/setup-python@v1
       with:
-        python-version: 3.9
+        python-version: 3.12
 
     - name: Update version
       run: sed -i "s/^version = .*/version = '${{github.ref_name}}'/" pyproject.toml

.readthedocs.yaml
@@ -6,7 +6,7 @@ sphinx:
 build:
   os: "ubuntu-22.04"
   tools:
-    python: "3.8"
+    python: "3.12"
   jobs:
     post_create_environment:
       # Install poetry

tox.ini
@@ -9,17 +9,18 @@ extend-ignore=E203,RST303
 [tox]
 isolated_build = True
 envlist =
-    py38-min
-    {py38,py39,py310,py311,py312}-pypi
+    py39-min
+    {py39,py310,py311,py312,py313,py314}-pypi
     pre-commit
 
 [gh-actions]
 python =
-    3.8: py38-min,py38-pypi
-    3.9: py39-pypi
+    3.9: py39-min,py39-pypi
     3.10: py310-pypi
-    3.11: py311-pypi,pre-commit
-    3.12: py312-pypi
+    3.11: py311-pypi
+    3.12: py312-pypi,pre-commit
+    3.13: py313-pypi
+    3.14: py314-pypi
 
 [testenv]
 setenv=PYTHONPATH = {toxinidir}:{toxinidir}

commit 0bf9d1fc2829427211133160fd6e7be86da4b2c2

Fix Python 3.14 test suite (#2090)
Typo introduced in https://github.com/spec-first/connexion/pull/2088,
which was silently ignored by Github.

.github/workflows/pipeline.yml
@@ -9,7 +9,7 @@ jobs:
     runs-on: ubuntu-latest
     strategy:
       matrix:
-        python-version: ['3.9', '3.10', '3.11', '3.12', '3.13', '3.14'']
+        python-version: ['3.9', '3.10', '3.11', '3.12', '3.13', '3.14']
     steps:
     - uses: actions/checkout@v2
     - name: Set up Python ${{ matrix.python-version }}

connexion/utils.py
@@ -7,8 +7,8 @@ import functools
 import importlib
 import inspect
 import os
-import pkgutil
 import sys
+import typing
 import typing as t
 
 import yaml
@@ -391,6 +391,7 @@ def coerce_type(param, value, parameter_type, parameter_name=None):
             return value
 
 
+@typing.no_type_check
 def get_root_path(import_name: str) -> str:
     """Copied from Flask:
     https://github.com/pallets/flask/blob/836866dc19218832cf02f8b04911060ac92bfc0b/src/flask/helpers.py#L595
@@ -398,6 +399,8 @@ def get_root_path(import_name: str) -> str:
     Find the root path of a package, or the path that contains a
     module. If it cannot be found, returns the current working
     directory.
+
+    :meta private:
     """
     # Module already imported and has a file attribute. Use that first.
     mod = sys.modules.get(import_name)
@@ -406,16 +409,24 @@ def get_root_path(import_name: str) -> str:
         return os.path.dirname(os.path.abspath(mod.__file__))
 
     # Next attempt: check the loader.
-    loader = pkgutil.get_loader(import_name)
+    try:
+        spec = importlib.util.find_spec(import_name)
+
+        if spec is None:
+            raise ValueError
+    except (ImportError, ValueError):
+        loader = None
+    else:
+        loader = spec.loader
 
     # Loader does not exist or we're referring to an unloaded main
     # module or a main module without path (interactive sessions), go
     # with the current working directory.
-    if loader is None or import_name == "__main__":
+    if loader is None:
         return os.getcwd()
 
     if hasattr(loader, "get_filename"):
-        filepath = loader.get_filename(import_name)  # type: ignore
+        filepath = loader.get_filename(import_name)  # pyright: ignore
     else:
         # Fall back to imports.
         __import__(import_name)
@@ -436,7 +447,7 @@ def get_root_path(import_name: str) -> str:
             )
 
     # filepath is import_name.py for a module, or __init__.py for a package.
-    return os.path.dirname(os.path.abspath(filepath))
+    return os.path.dirname(os.path.abspath(filepath))  # type: ignore[no-any-return]
 
 
 def inspect_function_arguments(function: t.Callable) -> t.Tuple[t.List[str], bool]:
@@ -542,5 +553,9 @@ def build_example_from_schema(schema):
     except ImportError:
         return None
 
-    faker = JSF(schema)
-    return faker.generate()
+    try:
+        faker = JSF(schema)
+        return faker.generate()
+    except TypeError:
+
+        return None

pyproject.toml
@@ -72,7 +72,7 @@ mock = ["jsf"]
 
 [tool.poetry.group.tests.dependencies]
 pre-commit = "~2.21.0"
-pytest = "7.2.1"
+pytest = "8.4.2"
 pytest-asyncio = "~0.18.3"
 pytest-cov = "~2.12.1"
 

tests/test_mock.py
@@ -1,3 +1,6 @@
+import sys
+
+import pytest
 from connexion.datastructures import NoContent
 from connexion.mock import MockResolver
 from connexion.operations import OpenAPIOperation, Swagger2Operation
@@ -195,6 +198,11 @@ def test_mock_resolver_example_nested_in_list_openapi():
     assert response == ["bar"]
 
 
+@pytest.mark.skipif(
+    sys.version_info >= (3, 14),
+    reason="This test fails on Python 3.14 due to lack of support from JSF dependency:"
+    "https://github.com/ghandic/jsf/issues/128.",
+)
 def test_mock_resolver_no_example_nested_in_object():
     resolver = MockResolver(mock_all=True)
 
@@ -229,6 +237,11 @@ def test_mock_resolver_no_example_nested_in_object():
     assert isinstance(response["foo"], str)
 
 
+@pytest.mark.skipif(
+    sys.version_info >= (3, 14),
+    reason="This test fails on Python 3.14 due to lack of support from JSF dependency:"
+    "https://github.com/ghandic/jsf/issues/128.",
+)
 def test_mock_resolver_no_example_nested_in_list_openapi():
     resolver = MockResolver(mock_all=True)
 

tests/test_mock2.py
@@ -1,7 +1,15 @@
+import sys
 from datetime import datetime
 from re import fullmatch
 
+import pytest
 from connexion.utils import build_example_from_schema
+
+pytestmark = pytest.mark.skipif(
+    sys.version_info >= (3, 14),
+    reason="This test fails on Python 3.14 due to lack of support from JSF dependency:"
+    "https://github.com/ghandic/jsf/issues/128.",
+)
 
 
 def test_build_example_from_schema_string():